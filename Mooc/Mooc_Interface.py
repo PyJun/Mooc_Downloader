f332b4dcbe81127dc0e4e76fd4de4d8296235718
PyJun <py.jun@qq.com>
2019-07-19 16:00:48 +0800

    upgrade to v3.4.1

--- Mooc/Mooc_Interface.py
+++ Mooc/Mooc_Interface.py
@@ -31,12 +31,12 @@ def mooc_interface():
         while True:
             os.system("cls")
             print("\t"+"="*91)
-            print('\t|\t\t\t  慕课视频下载器   \t\tQQ群: {:^27s} |'.format(__QQgroup__))
+            print('\t|\t\t      慕课下载器(免费版v3.4.1)     \tQQ群: {:^27s} |'.format(__QQgroup__))
             print("\t|\t\t    icourse163.org, icourses.cn    \t邮箱: {:^27s} |".format(__email__))
             print("\t"+"="*91)
-            print("\t\t\tGithub: https://github.com/PyJun/Mooc_Downloader")
-            print("\t\t\t博客: https://blog.csdn.net/qq_16166591/article/details/85249743")
-            print("\t{:^84}".format("下载路径: "+PATH))
+            print("\t{:^90}".format("Github: https://github.com/PyJun/Mooc_Downloader"))
+            print("\t{:^90}".format("博客: https://blog.csdn.net/qq_16166591/article/details/85249743"))
+            print("\t{:^90}".format("下载路径: "+PATH))
             urlstr = None
             while not urlstr:
                 try:
--- Mooc/__init__.py
+++ Mooc/__init__.py
@@ -1,12 +1,12 @@
 '''
-Mooc下载器版本  3.4.0
+Mooc下载器版本  3.4.1
 作者            PyJun
 邮箱            py.jun@qq.com
 github          https://github.com/PyJun/Mooc_Downloader
 博客            https://blog.csdn.net/qq_16166591/article/details/85249743
 '''
 
-__version__ = 'Mooc-3.4.0'
+__version__ = 'Mooc-3.4.1'
 __author__  = 'PyJun'
 __email__   = 'py.jun@qq.com'
 __github__  = 'https://github.com/PyJun/Mooc_Downloader'
